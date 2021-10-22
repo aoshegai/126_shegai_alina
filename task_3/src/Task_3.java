b931b39b0713e795f086efd8361b23cf64eb800e
aoshegai <aoshegai@gmail.com>
2021-10-22 21:55:30 +0300

    task_3

--- task_3/src/Task_3.java
+++ task_3/src/Task_3.java
@@ -3,7 +3,18 @@ public class Task_3 implements Task_3_base {
     public int subtask_1_for(int n1, int n2, int a, int b) {
         // подсчитать, сколько чисел, кратных a, но не кратных b,
         // находится между числами n1 и n2 включительно
-        return 0;
+        int count = 0;
+        int cn1 = n1;
+        if (n1 > n2) {
+            n1 = n2;
+            n2 = cn1;
+        }
+        for (int i = n1; i <= n2; i++) {
+            if ((i % a == 0) && (i % b != 0)) {
+                count++;
+            }
+        }
+        return count;
     }
 
     @Override
@@ -17,7 +28,16 @@ public class Task_3 implements Task_3_base {
         // ...
         // Найти, какое число будет находиться в этой последовательности
         // на позиции num
-        return 0;
+        int sum = 0;
+        for (int i = 1; i <= num; i++){
+            for (int j = 1; j <= i; j++) {
+                sum++;
+                if (sum == num) {
+                    return i;
+                }
+            }
+        }
+        return sum;
     }
 
     @Override
@@ -26,7 +46,13 @@ public class Task_3 implements Task_3_base {
         // a(0) = num
         // a(n) = a(n - 1) * d + 1
         // Найти сумму первых cnt элементов последовательности
-        return 0;
+        int a = num;
+        int sum = 0;
+        for (int i = 1; i <=cnt; i++) {
+            sum = sum + a;
+            a = a * d + 1;
+        }
+        return sum;
     }
 
     @Override
@@ -35,6 +61,12 @@ public class Task_3 implements Task_3_base {
         // S(n) = 1 + 1 * 2 + 1 * 2 * 3 + ... + n!
         // для заданного n
         // (n! - это n-факториал. Кто не знает - гуглите)
-        return 0;
+        int f = 1;
+        int sum = 0;
+        for (int i = 1; i <= n; i++){
+            f = f * i;
+            sum += f;
+        }
+        return sum;
     }
 }
