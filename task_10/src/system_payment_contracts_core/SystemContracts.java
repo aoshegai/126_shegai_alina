cae3bb86a9f0107be65138067fade6d054e4cd1c
aoshegai <aoshegai@gmail.com>
2022-01-10 23:04:17 +0300

    test complete

--- task_10/src/system_payment_contracts_core/SystemContracts.java
+++ task_10/src/system_payment_contracts_core/SystemContracts.java
@@ -1,10 +1,20 @@
 package system_payment_contracts_core;
 
 public class SystemContracts {
+    private int contractsCount;
+    private SystemContracts() {
+        contractsCount = 0;
+    }
+
+    public void addContract(String number, String date) {
+        contractsCount++;
+    }
+
     public static SystemContracts create() {
         return new SystemContracts();
     }
+
     public int getContractsCount() {
-        return 0;
+        return contractsCount;
     }
 }
