aad0c282c0413fe3c91f4db02a37df2866b3390a
aoshegai <aoshegai@gmail.com>
2022-01-10 21:50:52 +0300

    Создан тест create_CreatePaymentContracts_ContractsCountEqualsZero

---
+++ task_10/src/system_payment_contracts_core/SystemContracts.java
@@ -0,0 +1,10 @@
+package system_payment_contracts_core;
+
+public class SystemContracts {
+    public static SystemContracts create() {
+        return new SystemContracts();
+    }
+    public int getContractsCount() {
+        return 0;
+    }
+}
--- task_10/src/system_payment_contracts_tests/SystemPaymentContractsTests.java
+++ task_10/src/system_payment_contracts_tests/SystemPaymentContractsTests.java
@@ -1,6 +1,12 @@
 package system_payment_contracts_tests;
 
 import org.junit.*;
+import system_payment_contracts_core.*;
 
 public class SystemPaymentContractsTests extends Assert {
+    @Test
+    public void create_CreatePaymentContracts_ContractsCountEqualsZero() {
+        SystemContracts paymentContractsList = SystemContracts.create();
+        assertEquals(0, paymentContractsList.getContractsCount());
+    }
 }
