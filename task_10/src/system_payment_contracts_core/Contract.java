93ef4269d071fd51273b9bf2a60f8723498fa89e
aoshegai <aoshegai@gmail.com>
2022-01-11 01:09:42 +0300

    refactor code

---
+++ task_10/src/system_payment_contracts_core/Contract.java
@@ -0,0 +1,8 @@
+package system_payment_contracts_core;
+
+public class Contract {
+    private String date;
+    public Contract(String date) {
+        this.date = date;
+    }
+}
--- task_10/src/system_payment_contracts_core/SystemContracts.java
+++ task_10/src/system_payment_contracts_core/SystemContracts.java
@@ -1,19 +1,25 @@
 package system_payment_contracts_core;
 
+import java.util.HashMap;
+import java.util.ArrayList;
+
 public class SystemContracts {
-    private int contractsCount;
+    private HashMap<String, Contract> data;
     private SystemContracts() {
-        contractsCount = 0;
+        data = new HashMap<>();
     }
 
     public void addContract(String number, String date) {
-        if(number == null && date == null)
-            throw new IllegalArgumentException("Number can not be null, date can not be null");
+        StringBuilder error = new StringBuilder();
         if(number == null)
-            throw new IllegalArgumentException("Number can not be null");
+            error.append("Number can not be null\n");
         if(date == null)
-            throw new IllegalArgumentException("Date can not be null");
-        contractsCount++;
+            error.append("Date can not be null\n");
+        if(!error.isEmpty())
+            throw new IllegalArgumentException(error.toString());
+        if((!data.containsKey(number)) && (date.length() == 8)) {
+            data.put(number, new Contract(date));
+        }
     }
 
     public static SystemContracts create() {
@@ -21,6 +27,6 @@ public class SystemContracts {
     }
 
     public int getContractsCount() {
-        return contractsCount;
+        return data.size();
     }
 }
